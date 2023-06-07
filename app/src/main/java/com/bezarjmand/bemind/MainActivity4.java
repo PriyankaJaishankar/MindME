63d1eb3be1169e13433cf0d1986e873edfa6da0b
behzadarjmand <behzad.arjmand@gmail.com>
2023-06-07 14:52:26 +0200

    !!

--- app/src/main/java/com/bezarjmand/bemind/MainActivity4.java
+++ app/src/main/java/com/bezarjmand/bemind/MainActivity4.java
@@ -8,32 +8,31 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity4 extends AppCompatActivity {
-    private Button BreathingExercises, BodyScan, MindfulWalking;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
 
-        BreathingExercises = findViewById(R.id.BreathingExercises);
-        BodyScan = findViewById(R.id.BodyScan);
-        MindfulWalking = findViewById(R.id.MindfulWalking);
+        Button breathingExercises = findViewById(R.id.BreathingExercises);
+        Button bodyScan = findViewById(R.id.BodyScan);
+        Button mindfulWalking = findViewById(R.id.MindfulWalking);
 
-        BreathingExercises.setOnClickListener(new View.OnClickListener() {
+        breathingExercises.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity4.this, MainActivity5.class));
             }
         });
 
-        BodyScan.setOnClickListener(new View.OnClickListener() {
+        bodyScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity4.this, MainActivity6.class));
             }
         });
 
-        MindfulWalking.setOnClickListener(new View.OnClickListener() {
+        mindfulWalking.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity4.this, MainActivity7.class));
